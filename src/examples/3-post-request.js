b2618ec1cc2cdbae42bc2c79009f11c0b9d653d5
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-07 00:11:20 -0500

    POST Request

--- src/examples/3-post-request.js
+++ src/examples/3-post-request.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+
 const url = 'https://course-api.com/axios-tutorial-post';
 
 const PostRequest = () => {
@@ -8,38 +9,43 @@ const PostRequest = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email);
+    try {
+      const response = await axios.post(url, { name: name, email: email });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
     <section>
-      <h2 className='text-center'>post request</h2>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <h2 className="text-center">post request</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
           <input
-            type='text'
-            className='form-input'
-            id='name'
+            type="text"
+            className="form-input"
+            id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             email
           </label>
           <input
-            type='email'
-            className='form-input'
-            id='email'
+            type="email"
+            className="form-input"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           login
         </button>
       </form>
