f59f6624aed9ce9579d974389414fa79f5efd9c1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-08 02:08:21 -0500

    Global Default

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,17 +1,24 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const productsResponse = await axios(productsUrl);
+      const randomUserUrlResponse = await axios(randomUserUrl);
+      console.log(productsResponse);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
