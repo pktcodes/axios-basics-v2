8adb8bc4f844a5530f457d741b3f88076791c4c4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-09 14:32:34 -0500

    Custom Instance

--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,25 @@
 import { useEffect } from 'react';
 
+import authFetch from '../axios/custom';
+import axios from 'axios';
+
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const productsResponse = await authFetch('/react-store-products');
+      const randomuserResponse = await axios(randomUserUrl);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
+
 export default CustomInstance;
