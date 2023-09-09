80d96f37eba98fd37ebc2d7768f38ddb43076277
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-09-09 15:01:57 -0500

    Interceptors

---
+++ src/axios/interceptors.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const authFetch = axios.create({
+  baseURL: 'https://course-api.com/',
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers['Accept'] = 'application/json';
+    console.log('request sent');
+
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => {
+    console.log('got response');
+
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+    if (error.response.status === 404) {
+      // Do Something
+      console.log('NOT FOUND');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default authFetch;
--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,20 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const url = 'https://course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const productsResponse = await authFetch('/react-store-productss');
+    } catch (error) {}
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
+
 export default Interceptors;
